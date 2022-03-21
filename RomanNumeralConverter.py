d70d812ecaf226d74229232aa7ce3fc227811a8d
Joel Sommerfeld <joel.sommerfeld36@gmail.com>
2022-03-21 16:18:27 -0500

    Initial outline for a Roman numeral converter

---
+++ RomanNumeralConverter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  3 08:31:45 2022
+
+@author: joelsommerfeld
+"""
+
+# Roman Numeral Converter
+
+rn = input("Enter a Roman numeral: ")
+
+for num in rn:
+    it = num.split()
+    print(it)
+
+# Split and determine numeric value
+
+# Sum all values together
+
+# Print results out to the console
