da373bef07b0468ecd2997e0c4881c6d6ed5df6c
Joel Sommerfeld <joel.sommerfeld36@gmail.com>
2024-07-02 15:37:42 -0500

    Coloured print statements

--- timeCalculator.py
+++ timeCalculator.py
@@ -9,6 +9,11 @@ Created on Tue Jul  2 14:59:48 2024
 
 import re
 
+purple_color_code = "\033[35m"
+green_color_code = "\033[32m"
+reset_color_code = "\033[0m"
+
+
 def split_numbers_letters(s):
     # Use regex to find numbers and letters separately
     match = re.match(r"([0-9]+)([a-zA-Z]+)", s)
@@ -16,8 +21,8 @@ def split_numbers_letters(s):
         return match.groups()
     return None
 
-dist = input("What is the distance? \n")
-speed = input("What speed are you travelling at? \n")
+dist = input(f"{purple_color_code}What is the distance? \n {reset_color_code}")
+speed = input(f"{purple_color_code}What speed are you travelling at? \n {reset_color_code}")
 
 # Clean any units off the inputs and convert to numeric 
 dist_measure, dist_units = split_numbers_letters(dist)
@@ -26,4 +31,6 @@ speed_measure, speed_units = split_numbers_letters(speed)
 # speed = distance /time
 time = int(dist_measure)/int(speed_measure) * 60
 
-print(f"It would take {time} minutes to travel {dist} at {speed}")
+time = round(float(time), 2)
+
+print(f"{green_color_code}It would take {time} minutes to travel {dist} at {speed}{reset_color_code}")
